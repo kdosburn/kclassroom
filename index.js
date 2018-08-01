9480b709a9424b6bdaf4a454c808f940d89ccceb
Kendra Osburn <kendraosburn@Kendras-MacBook-Pro.local>
2018-08-01 13:47:27 -0700

    Connecting mLab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const generatePassword = require('password-generator');
+const secrets = require('./secrets');
 
 const app = express();
+const router = express.Router();
+
+mongoose.connect(secrets['dbUri']);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.get('/students', (req, res) => {
+  Student.find((err, students) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: students });
+  });
+});
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
---
+++ secrets.js
@@ -0,0 +1,7 @@
+// secrets.js
+const secrets = {
+  dbUri: 'mongodb://kendra:colin8wetfood@ds143099.mlab.com:43099/kendras-comment-box'
+};
+
+module.exports = secrets;
+
