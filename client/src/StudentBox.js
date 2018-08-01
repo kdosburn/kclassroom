1fd4c17f946b9ef776ed00819daf118a0fd6d7e2
Kendra Osburn <kendraosburn@Kendras-MacBook-Pro.local>
2018-08-01 11:23:07 -0700

    Testing es6

---
+++ client/src/StudentBox.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+// import StudentList from './StudentList';
+// import './StudentBox.css';
+
+class StudentBox extends Component {
+	constructor() {
+		super();
+		this.state = { 
+			data: [],
+			error: null,
+			firstName: '',
+			lastName: ''
+		};
+	}
+
+	componentDidMount() {
+		// this.loadStudentsFromServer();
+	}
+
+	loadStudentsFromServer = () => {
+		fetch('/api/averages')
+			.then(data => data.json())
+			.then((res) => {
+				console.log(res.data);
+				if(!res.success) this.setState({ error: res.error });
+				else this.setState({ data: res.data });
+			});
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="students">
+					<h2> STUDENTS: </h2>
+					
+				</div>
+			</div>
+		);
+	}
+
+}
+
+export default StudentBox;
