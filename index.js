c05217956dea52397de922b91f552d4860d0129d
Kendra Osburn <kendraosburn@Kendras-MacBook-Pro.local>
2018-08-01 21:17:10 -0700

    Fixed mLab collection error

--- index.js
+++ index.js
@@ -14,30 +14,36 @@ mongoose.connect(secrets['dbUri']);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.get('/students', (req, res) => {
-  Student.find((err, students) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: students });
-  });
+router.get('/', (req, res) => {
+	res.json({ message: 'Hello, World!' });
 });
+
+// router.get('/students', (req, res) => {
+// 	console.log('getting here!!');
+//   Student.find((err, students) => {
+//     if (err) return res.json({ success: false, error: err });
+//     return res.json({ success: true, data: students });
+//   });
+// });
 
 router.get('/averages', (req, res) => {
   Average.find((err, average) => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true, data: average });
   }).sort('studentId');
-  return res.json({error: true, message: "oh shoot" });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
+
+app.use('/api', router);
 
 console.log(`Listening on ${port}`);
--- secrets.js
+++ secrets.js
@@ -1,7 +1,6 @@
 // secrets.js
 const secrets = {
-  dbUri: 'mongodb://kendra:colin8wetfood@ds143099.mlab.com:43099/kendras-comment-box'
+  dbUri: 'mongodb://kendra:colin8wetfood@ds139970.mlab.com:39970/csgrades'
 };
 
 module.exports = secrets;
-
