0e02bf6aaa8ea6afc5e22ca14ef6eaffe66eabd1
Kendra Osburn <kendraosburn@Kendras-MacBook-Pro.local>
2018-08-01 21:28:33 -0700

    Testing endpoints

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.connect(secrets['dbUri']);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -37,9 +37,9 @@ router.get('/averages', (req, res) => {
   }).sort('studentId');
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
